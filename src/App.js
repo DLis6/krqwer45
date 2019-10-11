478df819f46f4ed732067190c89ec6617de2d4cc
David <54381677+DLis6@users.noreply.github.com>
2019-10-11 18:49:00 -0500

    TareasCompletas

--- src/App.js
+++ src/App.js
@@ -7,23 +7,61 @@ class App extends Component {
     super()
     this.state = {
       tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
+        { id: 1, name: "Sacar la ropa", done:false },
+        { id: 2, name: "Hacer la cama", done:true },
+        { id: 3, name: "Leer un rato", done:false }
       ],
       newTask: ''
     }
+    this.text = this.text.bind(this);
+    this.keyPress = this.keyPress.bind(this);
   }
+  
+  text(e) {
+    this.setState({ newTask: e.target.value });
+  }
+
+  keyPress(e) {
+    e.preventDefault();
+    if(this.state.newTask){ 
+     this.setState({
+       tasks: this.state.tasks.concat([{
+        id:this.state.tasks.length+1, 
+        name:this.state.newTask,
+        done:false
+        }]),
+       newTask:''
+     })
+    } 
+  }
+
+  tachar(id){
+ const newTasks= this.state.tasks.filter(newelem=>{
+
+    if(newelem.id === id){
+      newelem.done=!newelem.done
+    }
+    return newelem
+  })
+  this.setState({
+    tasks:newTasks
+  });
+  }
+
   render() {
+    ///const{newTask,tasks}=this.state; => Para evitar el this.state en el return
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li className={task.done?'done':''} key={task.id} onClick={(e)=>this.tachar(task.id)}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.keyPress}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" 
+            className={this.state.newTask?'':'error'}
+            value={this.state.newTask}
+            onChange={this.text}/>
           </form>
         </div>
       </div>
